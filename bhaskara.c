e5f5ae1d1ba0c614d00851edfe2584f38ed286c0
Italo Ramos <itaperam@gmail.com>
2020-03-28 00:21:08 -0300

    URI 1036

--- bhaskara.c
+++ bhaskara.c
@@ -3,14 +3,21 @@
 
 int main(){
 
-    double A=0, B=0, C=0, R1=0, R2=0;
+    double A=0, B=0, C=0, R1=0, R2=0, D=0;
 
-    scanf("%lf %lf %lf", &A, &B, &C);
+    scanf("%lf %lf %lf", &A, &B, &C);    
 
-    //R1 = ((-(B) - (sqrt(C)))/(2*A);
-    //R2 = ((-(B) + (sqrt(C)))/(2*A);
+    D = pow(B,2) - (4*A*C);
 
-    printf("R1 = %.5lf\nR2 = %.5lf\n", R1, R2);
+    R1 = (-B + sqrt(D))/(2*A);
+    R2 = (-B - sqrt(D))/(2*A);   
+
+    if(A == 0 || D < 0){
+        printf("Impossivel calcular\n");
+    }
+    else{
+        printf("R1 = %.5lf\nR2 = %.5lf\n", R1, R2);
+    }
 
     return 0;
 }
