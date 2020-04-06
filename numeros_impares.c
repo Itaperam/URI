a561179fe42bf60ec2ca8339ecb3608d15ffd866
Italo Ramos <itaperam@gmail.com>
2020-04-06 00:06:32 -0300

    URI 1067

---
+++ numeros_impares.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int num=0,i=0;
+
+    scanf("%d",&num);
+
+    for(i=1; i <= num; i++){
+        
+        if(i % 2 != 0)
+            printf("%d\n", i);        
+        
+    }    
+    return 0;
+}
