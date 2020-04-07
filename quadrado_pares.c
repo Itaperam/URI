0486a21d61491dd56d5d6c2edba2a5615a4fca6b
Italo Ramos <itaperam@gmail.com>
2020-04-07 20:24:10 -0300

    URI 1073

---
+++ quadrado_pares.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n =0, x=0, val=0;
+
+    scanf("%d", &n);
+
+    for(x=1; x <= n; x++){        
+
+        if(x % 2 == 0){
+            val = x * x;
+             printf("%d^2 = %d\n", x, val);
+        }        
+    }    
+    
+    return 0;
+}
