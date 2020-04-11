cd1e661c06328570144152ad667bb2884cb6abfa
Italo Ramos <itaperam@gmail.com>
2020-04-11 12:03:45 -0300

    URI 1079

---
+++ medias_ponderadas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n, i;
+
+    scanf("%d", &n);
+    
+    double media, n1, n2, n3;
+
+    for(i = 0; i < n; i++){
+        scanf("%lf %lf %lf", &n1, &n2, &n3);
+        media = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / (2+3+5);  
+        printf("%.1lf\n", media);                        
+    }    
+    return 0;
+}
