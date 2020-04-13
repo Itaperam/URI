8ac6a89ba540461f3bf81ed936a679d059a76a52
Italo Ramos <itaperam@gmail.com>
2020-04-13 15:15:56 -0300

    URI 1097

---
+++ sequenciaIJ_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int i=1, j=7;
+
+    for(i=1; i <= 9; j--){ 
+        
+        printf("I=%d J=%d\n", i, j);                
+
+        if(j - i == 4){
+            j = j + 5;
+            i = i+2;
+        }       
+            
+    } 
+    return 0;
+}
