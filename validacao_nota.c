57a36a524a302d99745275b17fdcb4c25e6bdef7
Italo Ramos <itaperam@gmail.com>
2020-04-18 12:35:20 -0300

    URI 1117

---
+++ validacao_nota.c
@@ -0,0 +1,27 @@
+//URI Online Judge | 1117
+#include <stdio.h>
+
+int main(){
+    double n, n1=-1, n2=-1, media;
+    int j=-1;
+
+    while(j!= 0){
+        scanf("%lf", &n);
+
+        if(n < 0 || n >10){
+            printf("nota invalida\n");
+        }
+        else{
+            if(n1 == -1){
+                n1 = n;
+            }
+            else if(n1 != -1 && n2 == -1){
+                n2 = n;
+                media = (n1+n2)/2;
+                printf("media = %.2lf\n", media);
+                j++;               
+            }
+        }        
+    }
+    return 0;
+}
