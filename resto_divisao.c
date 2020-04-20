4b16a84bef142da9ad5be63e3ca48d2bb6b0010f
Italo Ramos <itaperam@gmail.com>
2020-04-20 13:24:07 -0300

    URI 1133

---
+++ resto_divisao.c
@@ -0,0 +1,25 @@
+//URI Online Judge | 1133
+#include <stdio.h>
+
+int main(){
+    int x, y, i=0;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    if(x < y){
+        for(i = x+1; i < y; i++){
+            if(i % 5 == 2 || i % 5 == 3){
+                printf("%d\n", i);
+            }
+        } 
+    }
+    if(x > y){
+        for(i = y+1; i < x; i++){
+            if(i % 5 == 2 || i % 5 == 3){
+                printf("%d\n", i);
+            }
+        } 
+    }
+    return 0;
+}
