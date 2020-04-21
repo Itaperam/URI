fc6cb478aac3fc63c39711113a53f4503ef67244
Italo Ramos <itaperam@gmail.com>
2020-04-21 14:33:31 -0300

    URI 1143

---
+++ quadrado_cubo.c
@@ -0,0 +1,13 @@
+//URI Online Judge | 1143
+#include <stdio.h>
+
+int main(){
+    int n, i;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++){
+       printf("%d %d %d\n", i, i*i, i*i*i);        
+    }
+    return 0;
+}
