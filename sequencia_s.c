5b884bfc7eb1ac6fa2a0737d3449ea1559c8ca29
Italo Ramos <itaperam@gmail.com>
2020-05-02 11:30:27 -0300

    URI 1155

---
+++ sequencia_s.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    double s=0;
+
+    for(i = 1; i <=100; i++){
+        s += (double)1/i;       
+    }
+    printf("%.2lf\n", s);
+
+    return 0;
+}
